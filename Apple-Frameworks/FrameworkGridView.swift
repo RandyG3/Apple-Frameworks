160a3827a439a17930c2c457beaf0dd553d739e8
RandyG3 <40631249+RandyG3@users.noreply.github.com>
2023-12-29 21:22:25 -0500

    Framework Grid View Completed

--- Apple-Frameworks/FrameworkGridView.swift
+++ Apple-Frameworks/FrameworkGridView.swift
@@ -8,8 +8,23 @@
 import SwiftUI
 
 struct FrameworkGridView: View {
+    
+    let columns: [GridItem] = [GridItem(.flexible()),
+                               GridItem(.flexible()),
+                               GridItem(.flexible())
+    ]
+    
     var body: some View {
-        FrameworkTitleView(name: "App Clips", imageName: "app-clip")
+        NavigationView {
+            ScrollView {
+                LazyVGrid(columns: columns) {
+                    ForEach(MockData.frameworks) { framework in
+                        FrameworkTitleView(framework: framework)
+                    }
+                }
+            }
+            .navigationTitle("🍏 Frameworks")
+        }
     }
 }
 
@@ -18,19 +33,20 @@ struct FrameworkGridView: View {
 }
 
 struct FrameworkTitleView: View {
-    let name: String
-    let imageName: String
+
+    let framework: Framework
     
     var body: some View {
         VStack {
-            Image("app-clip")
+            Image(framework.imageName)
                 .resizable()
                 .frame(width: 90, height: 90)
-        Text("App Clips")
+            Text(framework.name)
                 .font(.title2)
                 .fontWeight(.semibold)
                 .scaledToFit()
                 .minimumScaleFactor(0.6)
         }
+        .padding()
     }
 }
